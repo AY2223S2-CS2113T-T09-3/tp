392d14fd0f95b8326184b737d77e710136c3f18f
aaronxujiachen <1943999635x@gmail.com>
2023-03-26 11:28:44 +0800

    Updated RecipePossibleCommandTest.java

--- src/test/java/seedu/mealcompanion/command/misc/RecipePossibleCommandTest.java
+++ src/test/java/seedu/mealcompanion/command/misc/RecipePossibleCommandTest.java
@@ -17,17 +17,22 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class RecipePossibleCommandTest {
     private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
     private final PrintStream originalOut = System.out;
+    private final PrintStream originalErr = System.err;
 
     @BeforeEach
     public void setUpStreams() {
         System.setOut(new PrintStream(outContent));
+        System.setErr(new PrintStream(errContent));
     }
 
     @AfterEach
     public void restoreStreams() {
         System.setOut(originalOut);
+        System.setErr(originalErr);
     }
+
 
     @Test
     public void recipePossibleAndRecipeNotPossible() throws MealCompanionException {
